d1d5b7063508fb7756cb790edcae97b54325f716
Liam YU <liamyu@live.com>
2020-03-31 22:55:36 -0700

    lc-142

--- LeetCode/Solutions2/FindDuplicatesSolution.cs
+++ LeetCode/Solutions2/FindDuplicatesSolution.cs
@@ -1,10 +1,37 @@
 namespace LeetCode.Csharp.Solutions2
 {
+    using System;
     using System.Collections.Generic;
 
     public class FindDuplicatesSolution
     {
+        // 442 - https://leetcode.com/problems/find-all-duplicates-in-an-array/
         public IList<int> FindDuplicates(int[] nums)
+        {
+            List<int> ret = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[Math.Abs(nums[i]) - 1] < 0)
+                {
+                    ret.Add(Math.Abs(nums[i]));
+                }
+                else
+                {
+                    nums[Math.Abs(nums[i]) - 1] = -nums[Math.Abs(nums[i]) - 1];
+                }
+            }
+
+            return ret;
+        }
+        
+        // 287 - https://leetcode.com/problems/find-the-duplicate-number/
+        public int FindDuplicate(int[] nums)
+        {
+            return -1;
+        }
+        
+        public IList<int> FindDuplicates2(int[] nums)
         {
             List<int> ret = new List<int>();
 
---
+++ LeetCode/Solutions2/HasCycleSolution.cs
@@ -0,0 +1,60 @@
+namespace LeetCode.Csharp.Solutions2
+{
+    using System;
+    using LeetCode.Csharp.Common;
+
+    public class HasCycleSolution
+    {
+        // 142 - https://leetcode.com/problems/linked-list-cycle-ii/
+        public ListNode DetectCycle(ListNode head)
+        {
+            // Answer: When slow meets fast, we have:
+            //     s_steps = before_loop + n * loop_size.
+            //     f_steps = before_loop + m * loop_size.
+            //     s_steps * 2 = f_steps;
+            // so:
+            //     f_steps = s_steps + (m - n) * loop_size.
+            //     s_steps = (m - n) * loop_size;
+            //
+            // Hence s_steps + before_loop = before_loop + (m - n) * loop_size;
+            
+            ListNode slow = head;
+            ListNode fast = head;
+            while (slow != null && fast != null)
+            {
+                slow = slow.next;
+                fast = fast.next?.next;
+                if (slow == fast && slow != null)
+                {
+                    ListNode hPtr2 = head;
+                    while (hPtr2 != slow)
+                    {
+                        hPtr2 = hPtr2.next;
+                        slow = slow.next;
+                    }
+
+                    return hPtr2;
+                }
+            }
+            
+            return null;
+        }
+
+        // 141 - https://leetcode.com/problems/linked-list-cycle/
+        public bool HasCycle(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+            while (slow != null && fast != null)
+            {
+                slow = slow.next;
+                fast = fast.next?.next;
+                if (slow == fast && slow != null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
