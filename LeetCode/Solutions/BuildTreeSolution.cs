7cc62ea61aa213c119a02ef7a6fae86b455a67aa
Liam YU <liamyu@live.com>
2020-04-20 09:38:06 -0700

    lc-1008

--- LeetCode/Solutions/BuildTreeSolution.cs
+++ LeetCode/Solutions/BuildTreeSolution.cs
@@ -11,7 +11,7 @@ namespace LeetCode.Csharp.Solutions
         public TreeNode BuildTree(int[] inorder, int[] postorder)
         {
             if (inorder.Length == 0) return null;
-            
+
             // This solution is all about partitioning.
             return this.BuildTreeInner(new Stack<int>(postorder), inorder, 0, inorder.Length - 1);
         }
@@ -32,18 +32,19 @@ namespace LeetCode.Csharp.Solutions
                     return node;
                 }
             }
-            
+
             throw new Exception("Unexpected input");
         }
-        
+
         // 105 - https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
-        public TreeNode BuildTree2(int[] preorder, int[] inorder) {
+        public TreeNode BuildTree2(int[] preorder, int[] inorder)
+        {
             if (inorder.Length == 0) return null;
-            
+
             // This solution is all about partitioning.
             return this.BuildTreeInner2(new Stack<int>(preorder.Reverse()), inorder, 0, inorder.Length - 1);
         }
-        
+
         private TreeNode BuildTreeInner2(Stack<int> roots, int[] nodes, int l, int r)
         {
             int currRoot = roots.Pop();
@@ -60,8 +61,41 @@ namespace LeetCode.Csharp.Solutions
                     return node;
                 }
             }
-            
+
             throw new Exception("Unexpected input");
+        }
+
+        // 1008 - https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+        public TreeNode BstFromPreorder(int[] preorder)
+        {
+            if (preorder.Length == 0) return null;
+            TreeNode root = new TreeNode(preorder[0]);
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            for (int i = 1; i < preorder.Length; i++)
+            {
+                int node = preorder[i];
+                TreeNode currNode = stack.Peek();
+                while (stack.Count > 0 && stack.Peek().val < node) // This makes the sub-tree
+                {
+                    // So currNode is a node bigger than nodeVal, or node whose parent is bigger than nodeVal.
+                    currNode = stack.Pop();
+                }
+
+                if (currNode.val > node)
+                {
+                    currNode.left = new TreeNode(node);
+                    stack.Push(currNode.left);
+                }
+                else
+                {
+                    currNode.right = new TreeNode(node);
+                    stack.Push(currNode.right);
+                }
+            }
+
+            return root;
         }
     }
 }
