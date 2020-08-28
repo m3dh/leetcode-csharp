6d24501dff9f1a5e112f056960856be7083d80ef
Liam Yu <liamyu@live.com>
2020-08-28 15:32:48 -0700

    checkin

---
+++ LeetCode/Solutions2/Random.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Csharp.Solutions2
+{
+    class Random
+    {
+        private int Rand7()
+        {
+            return -1;
+        }
+
+        // https://leetcode.com/problems/implement-rand10-using-rand7/
+        public int Rand10()
+        {
+            while (true)
+            {
+                // 等概率的 1 - 49!
+                int num = (Rand7() - 1) * 7 + Rand7();
+                if (num <= 40)
+                {
+                    return num % 10 + 1;
+                }
+            }
+        }
+    }
+}
