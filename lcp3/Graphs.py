d2e1e3a8a49b7d8b1bbe46c798c34f636ade666d
Liam YU <liamyu@live.com>
2020-08-30 10:01:59 -0700

    checkin

--- lcp3/Graphs.py
+++ lcp3/Graphs.py
@@ -1,5 +1,6 @@
 from typing import List
 from collections import deque, defaultdict
+from math import sqrt
 from pprint import pprint
 
 class UnionFindSet:
@@ -30,7 +31,7 @@ class Solution:
         for edge in edges:
             if not ufs.union(edge[0], edge[1]): return False
         return True
-    
+
     # https://leetcode.com/problems/critical-connections-in-a-network/
     # IDEA: 桥边 - 从上一个节点来的边，如果从本节点回溯的最早节点[的层数]等于本节点，就是桥边 - 注意处理回边，因为需要把无向图的边处理成双向边
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
@@ -39,7 +40,7 @@ class Solution:
         for conn in connections:
             graph[conn[0]].append(conn[1])
             graph[conn[1]].append(conn[0])
-        
+
         # update canReachLevel
         def dfs(lvl: int, cur: int, parent: int, canReach: List[int]):
             canReach[cur] = lvl # can always reach current node
@@ -47,18 +48,18 @@ class Solution:
                 if t == parent: continue # ignore reverse edges
                 elif canReach[t] < 0: # not yet visited
                     dfs(lvl+1, t, cur, canReach)
-                
+
                 # update even when dfs didn't happen for 't' -> because previous level should have been visited already
                 if canReach[t] < canReach[cur]:
                     canReach[cur] = canReach[t]
-            
+
             if canReach[cur] == lvl and parent >= 0:
                 result.append([parent, cur])
 
         canReach = [-1] * n
         dfs(0, 0, -1, canReach)
         return result
-        
+
 if __name__ == "__main__":
     sol = Solution()
     print(sol.criticalConnections(4, [[0,1],[1,2],[2,0],[1,3]]))
